64f96c62fcde1848bbbd80a507b9947f2b5919fb
avinashchowdarym <151068758+avinashchowdarym@users.noreply.github.com>
2023-11-19 20:58:48 +0530

    update changes

--- src/components/imagedisplay/ImgDIs.js
+++ src/components/imagedisplay/ImgDIs.js
@@ -3,15 +3,15 @@
 import React from "react";
 import { useEffect, useState } from "react";
 const imgUrl = [
-  "https://picsum.photos/200/300",
-  "https://picsum.photos/200",
-  "https://picsum.photos/id/237/200/300",
-  "https://picsum.photos/seed/picsum/200/300",
-  "https://picsum.photos/200/300?grayscale",
-  "https://picsum.photos/200/300/?blur",
-  "https://picsum.photos/200/300/?blur=2",
-  "https://picsum.photos/200/300.jpg",
-  "https://picsum.photos/200/300.webp"
+  "https://picsum.photos/200/200",
+  "https://picsum.photos/id/236/200",
+  "https://picsum.photos/id/237/200/200",
+  "https://picsum.photos/seed/picsum/200/200",
+  "https://picsum.photos/200/200?grayscale",
+  "https://picsum.photos/200/200/?blur",
+  "https://picsum.photos/200/200/?blur=2",
+  "https://picsum.photos/200/200.jpg",
+  "https://picsum.photos/200/200.webp"
 ];
 
 export default function ImgDis() {
@@ -29,15 +29,15 @@ export default function ImgDis() {
   return (
     <div>
       <div className="gallery">
-        <img src={imgUrl[(index + 1) % 9]} style={{ padding: "10px 10px" }} />
-        <img src={imgUrl[(index + 2) % 9]} style={{ padding: "10px 10px" }} />
-        <img src={imgUrl[(index + 3) % 9]} style={{ padding: "10px 10px" }} />
-        <img src={imgUrl[(index + 4) % 9]} style={{ padding: "10px 10px" }} />
-        <img src={imgUrl[(index + 5) % 9]} style={{ padding: "10px 10px" }} />
-        <img src={imgUrl[(index + 6) % 9]} style={{ padding: "10px 10px" }} />
-        <img src={imgUrl[(index + 7) % 9]} style={{ padding: "10px 10px" }} />
-        <img src={imgUrl[(index + 8) % 9]} style={{ padding: "10px 10px" }} />
-        <img src={imgUrl[(index + 9) % 9]} style={{ padding: "10px 10px" }} />
+        <img src={imgUrl[(index + 1) % 9]} style={{ borderRadius:'15px',padding: "10px 10px" }} />
+        <img src={imgUrl[(index + 2) % 9]} style={{ borderRadius:'15px',padding: "10px 10px" }} />
+        <img src={imgUrl[(index + 3) % 9]} style={{ borderRadius:'15px',padding: "10px 10px" }} />
+        <img src={imgUrl[(index + 4) % 9]} style={{ borderRadius:'15px',padding: "10px 10px" }} />
+        <img src={imgUrl[(index + 5) % 9]} style={{ borderRadius:'15px',padding: "10px 10px" }} />
+        <img src={imgUrl[(index + 6) % 9]} style={{ borderRadius:'15px',padding: "10px 10px" }} />
+        <img src={imgUrl[(index + 7) % 9]} style={{ borderRadius:'15px',padding: "10px 10px" }} />
+        <img src={imgUrl[(index + 8) % 9]} style={{ borderRadius:'15px',padding: "10px 10px" }} />
+        <img src={imgUrl[(index + 9) % 9]} style={{ borderRadius:'15px',padding: "10px 10px" }} />
       </div>
     </div>
   );
